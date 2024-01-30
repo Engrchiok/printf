df71b5ae289b5405df7ddc6c552849b6e6bb2719
Chinemerem Okonkwo <engrchiok@gmail.com>
2024-01-30 23:28:54 +0100

    updated task 1

--- function0.c
+++ function0.c
@@ -51,30 +51,6 @@ int percpr(char perc)
 }
 
 /**
- * intdisp - function that prints %d and %i.
- * @a: function parameter.
- * Return: always variable z.
- */
-
-int intdisp(int a)
-{
-	int c, z;
-
-	z = 0;
-	if (a == 0)
-	{
-		return (z);
-	}
-	c = a % 10;
-	c = c + '0';
-	a = a / 10;
-	intdisp(a);
-	write(1, &c, 1);
-	z++;
-	return (z);
-}
-
-/**
  * formpr - function that prints the format string.
  * Return: always z.
  */
--- function1.c
+++ function1.c
@@ -35,3 +35,27 @@ int intpr(va_list p)
 		return (z);
 	}
 }
+
+/**
+ * intdisp - function that prints %d and %i.
+ * @a: function parameter.
+ * Return: always variable z.
+ */
+
+int intdisp(int a)
+{
+	int c, z;
+
+	z = 0;
+	if (a == 0)
+	{
+		return (z);
+	}
+	c = a % 10;
+	c = c + '0';
+	a = a / 10;
+	intdisp(a);
+	write(1, &c, 1);
+	z++;
+	return (z);
+}
